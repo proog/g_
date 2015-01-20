58ae88710bbf305fc1bdae19dec9b5b853be1b58
Per Mortensen <per@permortensen.com>
2015-01-20 23:58:59 +0100

    lol

--- api/modules/setup.php
+++ api/modules/setup.php
@@ -38,7 +38,7 @@ function doSetup() {
     $settings = decodeJsonOrFail(file_get_contents('../config/db.json'));
     $dsn = 'mysql:dbname='.$settings['database'].';host='.$settings['host'].';port='.$settings['port'];
     $createTables = file_get_contents('../config/create_tables.sql');
-    //try {
+    try {
         $con = new PDO($dsn, $settings['username'], $settings['password']);
         if($con->exec($createTables) === false)
             throw new Exception(implode(', ', $con->errorInfo()));
@@ -52,11 +52,11 @@ function doSetup() {
         $config = new Config();
         $config->default_user = $user->id;
         $config->save();
-    /*} catch(PDOException $e) {
+    } catch(PDOException $e) {
         $app->flashNow('db', 'Could not create tables for g_. The most likely reason for this is an incorrect database login or insufficient permissions for the database user.');
         showSetup();
         return;
-    }*/
+    }
 
     $app->redirect('../');
 }
