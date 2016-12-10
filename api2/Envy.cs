c912d49db5d3312a7a46306bb2b05d2fb74ae541
Per Mortensen <per@permortensen.com>
2016-12-10 02:23:56 +0100

    Removed unused code

--- api2/Envy.cs
+++ api2/Envy.cs
@@ -3,21 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Games {
     public static class Extensions {
-        public static T FindOrFail<T>(this DbSet<T> dbSet, params object[] keyValues) where T : class {
-            var x = dbSet.Find(keyValues);
-
-            if (x == null) {
-                throw new ModelNotFoundException();
-            }
-
-            return x;
-        }
-
         public static int ToUnixTimestamp(this DateTime dt) {
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             return (int) dt.Subtract(epoch).TotalSeconds;
         }
     }
-
-    public class ModelNotFoundException : Exception { }
 }
