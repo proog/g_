8af3b91e7ba37a62fdb6e914b10775921e8a5b2f
Per Mortensen <per@permortensen.com>
2017-10-22 01:26:11 +0200

    Update cloned descriptor list when parent prop changes

--- src/public2src/descriptor-list.js
+++ src/public2src/descriptor-list.js
@@ -7,6 +7,12 @@ Vue.component('descriptor-list', {
   data() {
     return { cloned: _.clone(this.value) }
   },
+  watch: {
+    value: function (newValue) {
+      // update cloned list when parent component has updated the original
+      this.cloned = _.clone(newValue)
+    }
+  },
   methods: {
     update() {
       this.$emit('input', this.cloned)
