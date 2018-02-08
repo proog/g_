79038336486f667112177c9b01df1cb24888de79
Per Mortensen <per@permortensen.com>
2018-02-08 22:29:22 +0100

    Make descriptor editors smaller

--- src/VueApp/src/DescriptorsEditor.vue
+++ src/VueApp/src/DescriptorsEditor.vue
@@ -3,27 +3,27 @@
   <div class="form-row" v-for="item in items" :key="item.id">
     <div class="col-6 form-group">
       <input type="text"
-              class="form-control"
+              class="form-control form-control-sm"
               required
               placeholder="Name"
               v-model="item.name">
     </div>
     <div class="col-4 form-group">
       <input type="text"
-              class="form-control"
+              class="form-control form-control-sm"
               required
               placeholder="Short name"
               v-model="item.short_name">
     </div>
     <div class="col-2 form-group">
-      <button type="button" class="btn btn-outline-danger" @click="remove(item)">
+      <button type="button" class="btn btn-sm btn-outline-danger" @click="remove(item)">
         <i class="fa fa-times"></i>
       </button>
     </div>
   </div>
   <div class="form-row">
     <div class="col form-group">
-      <button type="button" class="btn btn-outline-success" @click="add">
+      <button type="button" class="btn btn-sm btn-outline-success" @click="add">
         <i class="fa fa-plus"></i>
       </button>
     </div>
